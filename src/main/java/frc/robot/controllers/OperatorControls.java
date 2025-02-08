9c826c7522ffbcf3b2e1562305a6bb5e1db256b1
Steven <scarmain@gmail.com>
2025-02-08 12:57:19 -0500

    add deadband and slow down operator controls
    Co-authored-by: hello is my name <hello-isnt-available@users.noreply.github.com>

--- src/main/java/frc/robot/controllers/OperatorControls.java
+++ src/main/java/frc/robot/controllers/OperatorControls.java
@@ -1,21 +1,25 @@
 package frc.robot.controllers;
 
+import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import org.livoniawarriors.UtilFunctions;
 
 public class OperatorControls {
   private XboxController operator;
+  private DoubleSubscriber opDeadband;
 
   public OperatorControls() {
     operator = new XboxController(2);
+    opDeadband = UtilFunctions.getSettingSub("/Operator/Deadband", 0.05);
   }
 
   public double getElevatorRequest() {
-    return -operator.getLeftY();
+    return -UtilFunctions.deadband(operator.getLeftY(), opDeadband.get()) * 0.5;
   }
 
   public double getPivotRequest() {
-    return -operator.getRightY();
+    return -UtilFunctions.deadband(operator.getRightY(), opDeadband.get()) * 0.5;
   }
 
   public double getIntakeRequest() {
